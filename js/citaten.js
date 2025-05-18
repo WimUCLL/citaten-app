c0c60e9e41de852e8481f764dcc99262b65f1799
wim-va <wim.vanasch@student.ucll.be>
2025-05-18 17:19:26 +0200

    modulaire opbouw

---
+++ js/citaten.js
@@ -0,0 +1,18 @@
+export const citaten = [{
+  titel: 'To be',
+  citaat: 'To be or not to be, that\'s the question.',
+  auteur: 'William Shakespeare',
+  taal: 'en'
+},
+{
+  titel: 'Vandaag',
+  citaat: 'Wat je vandaag moet doen, moet je doen zoals je morgen denkt dat je het had moeten doen.',
+  auteur: 'Toon Hermans',
+  taal: 'nl'
+},
+{
+  titel: 'Penser',
+  citaat: 'Je pense, donc je suis.',
+  auteur: 'René Descartes',
+  taal: 'fr'
+}];
--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-// citaten
-const citaten = [{
-  titel: 'To be',
-  citaat: 'To be or not to be, that\'s the question.',
-  auteur: 'William Shakespeare',
-  taal: 'en'
-},
-{
-  titel: 'Vandaag',
-  citaat: 'Wat je vandaag moet doen, moet je doen zoals je morgen denkt dat je het had moeten doen.',
-  auteur: 'Toon Hermans',
-  taal: 'nl'
-},
-{
-  titel: 'Penser',
-  citaat: 'Je pense, donc je suis.',
-  auteur: 'René Descartes',
-  taal: 'fr'
-}]
+import { citaten } from './citaten.js';
 
 // h1 & p aanpassen
 document.querySelector('h1').innerText = 'Inspirerende citaten';
@@ -24,7 +6,7 @@ document.getElementById('geencitaten').remove();
 
 // Voeg citaten toe d.m.v.functie
 function voegCitaatObjectToe(citaatObject) {
-  artikel = document.createElement('article');
+  const artikel = document.createElement('article');
   artikel.innerHTML = `<h2>${citaatObject.titel}</h2><p>${citaatObject.citaat}</p><p class="auteur">${citaatObject.auteur}</p>`;
   artikel.classList.add(`${citaatObject.taal}`);
   document.querySelector('.citaten').insertAdjacentElement('beforeend', artikel);
