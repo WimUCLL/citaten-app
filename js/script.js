80cd1792411126ed3cf845a8fa2fc00b0c785a93
wim-va <wim.vanasch@student.ucll.be>
2025-05-18 20:44:24 +0200

    Statistieken geimplementeerd

--- js/script.js
+++ js/script.js
@@ -5,28 +5,17 @@ document.querySelector('h1').innerText = 'Inspirerende citaten';
 document.getElementById('geencitaten').remove();
 
 // Voeg citaten toe d.m.v.functie
+let citaatLengte = 0;
 function voegCitaatObjectToe(citaatObject) {
   const artikel = document.createElement('article');
   artikel.innerHTML = `<h2>${citaatObject.titel}</h2><p>${citaatObject.citaat}</p><p class="auteur">${citaatObject.auteur}</p>`;
   artikel.classList.add(`${citaatObject.taal}`);
   document.querySelector('.citaten').insertAdjacentElement('beforeend', artikel);
+  if (citaatObject.citaat.length > citaatLengte)
+    citaatLengte = citaatObject.citaat.length
 }
 
-// for loop
-/* 
-for (let i = 0; i < citaten.length; i++) {
-  voegCitaatObjectToe(citaten[i]);
-}
-*/
-
-// for ... of ... loop
-/* 
-for (const citaat of citaten) {
-  voegCitaatObjectToe(citaat);
-}
-*/
-
-// forEach (_iz nice!!_)
+// toon citaten
 citaten.forEach(voegCitaatObjectToe);
 
 // forEach fat arrow
@@ -42,3 +31,35 @@ kolommenKnop.addEventListener('click', () => {
 
 // footer
 document.body.insertAdjacentHTML('beforeend', '<footer><p>&copy; 2025</p></footer>');
+
+// statistieken
+/* function citatenLengte() {
+  let langsteCitaat = '';
+  for (const cit of citaten) {
+    if (cit.citaat.length > langsteCitaat.length)
+      langsteCitaat = cit;
+  }
+  return langsteCitaat;
+}
+const langsteCitaatTekst = citatenLengte()
+ */
+
+const langsteCitaatZoeker = () => {
+  let langste = '';
+  for (const ciz of citaten) {
+    console.log(ciz)
+    if (langste.length < ciz.citaat.length) {
+      langste = ciz;
+    }
+  }
+  return langste;
+}
+
+const langsteCitaatTekst = langsteCitaatZoeker()
+
+
+// console.log(44, typeof langsteCitaatTekst())
+
+
+document.querySelector('#aantalCitaten').innerText += ' ' + citaten.length;
+document.querySelector('#langsteCitaat').innerText = `Langste citaat: (${langsteCitaatTekst.citaat.length} letters, auteur: ${langsteCitaatTekst.auteur}) "${langsteCitaatTekst.citaat}"`;
